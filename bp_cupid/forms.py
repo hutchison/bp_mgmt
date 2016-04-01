93aaa74fefc7ce8ddc543de98d6d96e9d038e49b
Martin Darmüntzel <martin@trivialanalog.de>
2016-04-01 18:16:54 +0200

    Fügt Formular für Evaluation hinzu.
    (cherry picked from commit 4a45649570ff38483986dd8550789686b555613c)

--- bp_cupid/forms.py
+++ bp_cupid/forms.py
@@ -1,10 +1,10 @@
-from django import forms
+from django.forms import ModelForm, Textarea, NumberInput, Select
 from django_ace import AceWidget
 
-from .models import Vorlage
+from .models import Vorlage, Evaluation
 
 
-class VorlagenForm(forms.ModelForm):
+class VorlagenForm(ModelForm):
     class Meta:
         model = Vorlage
         fields = ['token', 'text']
@@ -17,3 +17,22 @@ class VorlagenForm(forms.ModelForm):
                 wordwrap=True,
             ),
         }
+
+
+def bootstrap_field(f, **kwargs):
+    field = f.formfield(**kwargs)
+    if field and type(field.widget) in [Textarea, NumberInput, Select]:
+        field.widget.attrs['class'] = 'form-control'
+        if f.name.startswith('kommentar_'):
+            field.widget.attrs['rows'] = '2'
+    if field and type(field.widget) == NumberInput:
+        field.widget.attrs['min'] = '1'
+        field.widget.attrs['max'] = '6'
+    return field
+
+
+class EvaluationForm(ModelForm):
+    formfield_callback = bootstrap_field
+    class Meta:
+        model = Evaluation
+        fields = '__all__'
