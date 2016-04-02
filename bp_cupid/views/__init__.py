4899cd97c4b5f691fde128141fdc23ee8d0e537b
Martin Darmüntzel <martin@trivialanalog.de>
2016-04-02 18:07:32 +0200

    cleanup: räumt views/__init__.py auf.
    (cherry picked from commit a24bbf2da7d47479b03f9839f6ca6fe037a02f90)

--- bp_cupid/views/__init__.py
+++ bp_cupid/views/__init__.py
@@ -1,20 +1,20 @@
-from bp_cupid.views.index import index
-from bp_cupid.views.Status import Status
-from bp_cupid.views.gewichte import gewichte
+from .index import index
+from .Status import Status
+from .gewichte import gewichte
 from .Student import StudentDetail, StudentList
 from .Praxis import PraxisDetail, PraxisList
-from bp_cupid.views.Plaetze import Plaetze
-from bp_cupid.views.Platzuebersicht import Platzuebersicht
+from .Plaetze import Plaetze
+from .Platzuebersicht import Platzuebersicht
 from .verteilen import (
     zufaellig_verteilen,
     gezielt_verteilen,
 )
-from bp_cupid.views.studenten_zulassen import studenten_zulassen
-from bp_cupid.views.Bloecke import Bloecke
-from bp_cupid.views.email import email, preview_email
-from bp_cupid.views.zusatzinfo import zusatzinfo
-from bp_cupid.views.pdf import praxis as pdf_praxis
-from bp_cupid.views.pdf import praxen as pdf_praxen
-from bp_cupid.views import excel
+from .studenten_zulassen import studenten_zulassen
+from .Bloecke import Bloecke
+from .email import email, preview_email
+from .zusatzinfo import zusatzinfo
+from .pdf import praxis as pdf_praxis
+from .pdf import praxen as pdf_praxen
+from . import excel
 from .einstellungen import einstellungen
 from .Evaluation import EvaluationList, EvaluationDetail
