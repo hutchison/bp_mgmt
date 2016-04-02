99e81e3f02d28f129a4bc081b2465a7b6c0f84b0
Martin Darmüntzel <martin@trivialanalog.de>
2016-04-02 17:02:59 +0200

    cleanup: view: Studenten → StudentList, verschiebt alles in Student.py
    (cherry picked from commit 80b41872a9690a67bb32f764c250dd263badd859)

--- bp_cupid/views/Student.py
+++ bp_cupid/views/Student.py
@@ -44,3 +44,50 @@ class StudentDetail(View):
         }
 
         return render(request, self.template_name, context)
+
+
+class StudentList(View):
+    template_name = 'bp_cupid/studenten.html'
+
+    @method_decorator(login_required)
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def get(self, request):
+        """
+        Zeigt alle Studenten in einer Tabelle nach Nachname sortiert an.
+        """
+        akt_verw_zr = request.user.mitarbeiter.akt_verw_zeitraum
+
+        context = {
+            'studenten': Student.objects.filter(
+                verwaltungszeitraum=akt_verw_zr
+            ).select_related(
+                'platz'
+            ).prefetch_related(
+                'landkreise',
+                'bevorzugte_praxen',
+            ).order_by('name'),
+        }
+
+        anz_studis = Student.objects.filter(
+            verwaltungszeitraum=akt_verw_zr
+        ).count()
+
+        if anz_studis:
+            anz_studis_mit_fragebogen = Student.objects.filter(
+                verwaltungszeitraum=akt_verw_zr,
+                hat_fragebogen_ausgefuellt=True,
+            ).count()
+
+            rel_fragebogen = round(
+                100 * anz_studis_mit_fragebogen / anz_studis, 1
+            )
+
+            context.update(
+                {
+                    'anz_studis': anz_studis,
+                    'anz_studis_mit_fragebogen': anz_studis_mit_fragebogen,
+                    'rel_fragebogen': rel_fragebogen,
+                }
+            )
+
+        return render(request, self.template_name, context)
