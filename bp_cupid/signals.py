0ebe716f0afae58f203f65483dedbecaaea09178
Martin Darmüntzel <martin@trivialanalog.de>
2016-04-03 18:49:57 +0200

    cleanup: räumt bp_cupid.signals auf.
    (cherry picked from commit d1e58489bd211bc0eee443ea6970882e6737a2c4)

--- bp_cupid/signals.py
+++ bp_cupid/signals.py
@@ -1,13 +1,15 @@
 from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
-from bp_cupid.models import (
-    Praxis,
+
+from .models import (
     Platz,
+    Praxis,
     Zeitraum,
 )
 
 import logging
 logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=Praxis)
 def aktualisiere_zeitraeume_nach_praxisspeicherung(sender, instance, **kwargs):
@@ -29,6 +31,7 @@ m2m_changed.connect(
     sender=Praxis.zeitraeume.through,
 )
 
+
 @receiver(post_save, sender=Platz)
 def aktualisiere_zeitraeume_nach_platzspeicherung(sender, instance, **kwargs):
     praxis = instance.praxis
@@ -38,6 +41,7 @@ def aktualisiere_zeitraeume_nach_platzspeicherung(sender, instance, **kwargs):
     )
     praxis.aktualisiere_zeitraeume()
 
+
 @receiver(post_delete, sender=Platz)
 def aktualisiere_zeitraeume_nach_platzloeschung(sender, instance, **kwargs):
     praxis = instance.praxis
@@ -46,6 +50,7 @@ def aktualisiere_zeitraeume_nach_platzloeschung(sender, instance, **kwargs):
         'Signal post_delete von Platz erhalten. '
         'Aktualisiere Zeiträume von {}.'.format(praxis)
     )
+
 
 @receiver(post_save, sender=Zeitraum)
 def aktualisiere_ueberlappende_zeitraeume(sender, instance, **kwargs):
